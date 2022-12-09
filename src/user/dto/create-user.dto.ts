02b93af34f5ff946aac0fa2c8a38884ec4616dbb
Erick <santoserick9@gmail.com>
2022-12-09 20:30:22 -0300

    create user service and controller

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,24 @@
-export class CreateUserDto {}
+import {
+  IsEmail,
+  IsString,
+  MinLength,
+  MaxLength,
+  Matches,
+} from 'class-validator';
+import { User } from '../entities/user.entity';
+
+export class CreateUserDto extends User {
+  @IsString()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password too weak',
+  })
+  password: string;
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,49 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { hash } from 'bcrypt';
+
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto): Promise<Omit<User, 'password'>> {
+    const data: Prisma.UserCreateInput = {
+      ...createUserDto,
+      password: await hash(createUserDto.password, 10),
+    };
+
+    const { id, name, email } = await this.prisma.user.create({
+      data,
+    });
+
+    return {
+      id,
+      name,
+      email,
+    };
   }
 
-  findAll() {
-    return `This action returns all user`;
+  findByEmail(email: string) {
+    return this.prisma.user.findUnique({ where: { email } });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
+  // findAll() {
+  //   return `This action returns all user`;
+  // }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
+  // findOne(id: number) {
+  //   return `This action returns a #${id} user`;
+  // }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
-  }
+  // update(id: number, updateUserDto: UpdateUserDto) {
+  //   return `This action updates a #${id} user`;
+  // }
+
+  // remove(id: number) {
+  //   return `This action removes a #${id} user`;
+  // }
 }
